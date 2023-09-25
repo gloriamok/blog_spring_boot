4a10afc8da44aebeb291bd7201cf3bc419c9ac33
gloriamok <gloriamok15@gmail.com>
2023-09-25 19:02:57 +0900

    fix: Add Comment Entity and OneToMany Relationship

---
+++ src/main/java/com/blog_spring_boot/blog_spring_boot/comment/Comment.java
@@ -0,0 +1,28 @@
+package com.blog_spring_boot.blog_spring_boot.comment;
+
+import com.blog_spring_boot.blog_spring_boot.post.Post;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+}
--- src/main/java/com/blog_spring_boot/blog_spring_boot/post/Post.java
+++ src/main/java/com/blog_spring_boot/blog_spring_boot/post/Post.java
@@ -1,9 +1,13 @@
 package com.blog_spring_boot.blog_spring_boot.post;
 
+import com.blog_spring_boot.blog_spring_boot.comment.Comment;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -23,4 +27,7 @@ public class Post {
 
     @Column(name = "content", nullable = false)
     private String content;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    private Set<Comment> commentSet = new HashSet<>();
 }
