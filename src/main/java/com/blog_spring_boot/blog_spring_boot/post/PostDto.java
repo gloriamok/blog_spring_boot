ce5d3cbb68044319b2dcc423d7ce50823b770278
gloriamok <gloriamok15@gmail.com>
2023-09-26 15:21:41 +0900

    fix: Get Post along with its comments

--- src/main/java/com/blog_spring_boot/blog_spring_boot/post/PostDto.java
+++ src/main/java/com/blog_spring_boot/blog_spring_boot/post/PostDto.java
@@ -1,6 +1,9 @@
 package com.blog_spring_boot.blog_spring_boot.post;
 
+import com.blog_spring_boot.blog_spring_boot.comment.CommentDto;
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 public class PostDto {
@@ -8,4 +11,5 @@ public class PostDto {
     private String title;
     private String description;
     private String content;
+    private Set<CommentDto> commentSet;
 }
